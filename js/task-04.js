98542343859a7a60095423c2ca7da358bdac6ed9
Yevhen2701 <106544836+EvgenAltunin@users.noreply.github.com>
2022-10-08 16:56:24 +0300

    4 task  was completed

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const decrementBtnEl = document.querySelector('[data-action="decrement"]')
+const incrementBtnEl = document.querySelector('[data-action="increment"]')
+const valueEl = document.querySelector("#value")
+
+var counterValue = Number(valueEl.textContent)
+
+const onDecrementBtnElClick = () => {
+  counterValue -= 1;
+  valueEl.innerHTML = `${counterValue}`
+    console.log("Decrement button was clicked and the value was decremented by 1");
+
+};
+
+const onIncrementBtnElClick = () => {
+  console.log("Increment button was clicked and the value was incremented by 1");
+  counterValue += 1;
+  valueEl.innerHTML = `${counterValue}`
+};
+
+decrementBtnEl.addEventListener("click", onDecrementBtnElClick);
+incrementBtnEl.addEventListener("click", onIncrementBtnElClick);
