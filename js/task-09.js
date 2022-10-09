b7dc9e8861223c0ccffa5af4d0716aa577602aa9
Yevhen2701 <106544836+EvgenAltunin@users.noreply.github.com>
2022-10-09 21:48:52 +0300

    9 task was completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const body = document.body;
+const changeColorBtn = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", onChangeColorBtnClick);
+
+function onChangeColorBtnClick(event) {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
 }
